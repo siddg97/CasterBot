9b7cdcce57327898e5e4cc4c5b759fefe3181205
Siddharth Gupta <sga94@sfu.ca>
2019-05-05 18:08:54 -0700

    Added: $weather command__BASIC

--- bot.js
+++ bot.js
@@ -24,7 +24,7 @@ bot.on('ready', function (evt) {
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
+    if (message.substring(0, 1) == '$') {
         var args = message.substring(1).split(' ');
         var cmd = args[0];
        
@@ -35,6 +35,11 @@ bot.on('message', function (user, userID, channelID, message, evt) {
                 bot.sendMessage({
                     to: channelID,
                     message: 'looney!'
+                });
+            case 'weather':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'read command: $weather'
                 });
             break;
             // Just add any case commands if you want to..
